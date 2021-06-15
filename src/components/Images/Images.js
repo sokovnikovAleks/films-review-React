40572de201b3eb88fca11f008f7387ceab6a870b
Aleksandr Sokovnikov <Sokovnikov1984@yandex.ru>
2021-06-15 16:37:56 +0300

    show/hide images

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,10 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from './Images.module.css'
 import ModalImage from "../ModalImage/ModalImage";
 
 const Images = ({images}) => {
+  const [btnTitle, setBtnTitle] = useState('')
   const [showModal, setShowModal] = useState(false)
   const [currentImage, setCurrentImage] = useState(null)
+  const [shownImages, setShownImages] = useState([])
+  const [showAllImages, setShowAllImages] = useState(false)
+
+  useEffect(() => {
+    setShownImages(() => images.slice(0, 5))
+    if (images.length > 5) {
+      setBtnTitle('Показать все')
+    }
+  }, [])
+
+  useEffect(() => {
+    if(showAllImages) {
+      setShownImages(images)
+      setBtnTitle('Скрыть')
+    } else if(!showAllImages && images.length > 5) {
+      setShownImages(() => images.slice(0, 5))
+      setBtnTitle('Показать все')
+    }
+  }, [showAllImages])
 
   const openModal = (index) => {
     setCurrentImage(index)
@@ -14,13 +34,22 @@ const Images = ({images}) => {
 
   return (
     <div className={classes.container}>
-      {images.map((item, index) => {
-        return (
-          <div className={classes.imgContainer} key={item.preview} onClick={() => openModal(index)}>
-            <img className={classes.img} src={item.preview} alt="фото фильма"/>
-          </div>
-        )
-      })}
+
+      <div className={classes.imagesContainer}>
+        {shownImages.map((item, index) => {
+          return (
+            <div className={classes.imgContainer} key={item.preview} onClick={() => openModal(index)}>
+              <img className={classes.img} src={item.preview} alt="фото фильма"/>
+            </div>
+          )
+        })}
+      </div>
+
+      {btnTitle
+        ? <button className={classes.showBtn} onClick={() => setShowAllImages(prev => !prev)}>{btnTitle}</button>
+        : null
+      }
+
       {showModal ? <ModalImage closeModal={() =>setShowModal(false)} currentImage={currentImage} images={images} /> : null}
     </div>
   );
